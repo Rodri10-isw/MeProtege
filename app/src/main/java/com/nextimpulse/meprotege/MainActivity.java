ccb91c61b4fc4e2c081ee0bee2691b22482000c9
HP <roodry1008goo@gmail.com>
2020-08-26 12:24:18 -0500

    login tipo de usuatrios

--- app/src/main/java/com/nextimpulse/meprotege/MainActivity.java
+++ app/src/main/java/com/nextimpulse/meprotege/MainActivity.java
@@ -10,10 +10,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtemail,edtpass;
@@ -21,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     private String correo="", passw="";
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth=FirebaseAuth.getInstance();
+        mDatabase= FirebaseDatabase.getInstance().getReference();
         edtemail=(EditText)findViewById(R.id.edtCorreo);
         edtpass=(EditText)findViewById(R.id.edtContrasena);
         btnReg=(Button)findViewById(R.id.btnRegistrarse);
@@ -49,14 +57,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
         private void  loginUser(){
             mAuth.signInWithEmailAndPassword(correo,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        startActivity(new Intent(MainActivity.this,Inicio.class));
-                        finish();
+                        obtenerDatosUsr();
                     }else{
                         Toast.makeText(MainActivity.this, "Correo o contraseña incorrectos",Toast.LENGTH_SHORT).show();
                     }
@@ -64,6 +72,33 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+
+    private void obtenerDatosUsr(){
+        String id = mAuth.getCurrentUser().getUid();
+        mDatabase.child("Users").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String tipo=dataSnapshot.child("tipo").getValue().toString();
+                    if (tipo.equals("1")){
+                        Toast.makeText(MainActivity.this, "No te mamas "+tipo,Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this,Inicio.class));
+                        finish();
+                    }else{
+                        if (tipo.equals("repartidor"))
+                        Toast.makeText(MainActivity.this, "Te mamas "+tipo,Toast.LENGTH_SHORT).show();
+                    }
+
+                    //startActivity(new Intent(MainActivity.this,Inicio.class));
+                    //
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -73,3 +108,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
